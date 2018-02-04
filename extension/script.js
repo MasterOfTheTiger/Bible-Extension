80efcf30a540e72ee4581860eaa0a4c0253851d2
MasterOfTheTiger <MasterOfTheTiger>
2018-02-03 16:49:22 -0800

    Messed more stuff up

--- extension/script.js
+++ extension/script.js
@@ -1,12 +1,36 @@
-var url = "https://baseurl/recver.php?String='John 1:1'&Out=json";
+function getVerses(url) {
+  $.ajax({
+    url: url,
+    success: function(result){
+    //$("#reqObject").text(url);
+    //$("#returnObject").text(JSON.stringify(result, null, 4));
+    $("#demo").text(result.verses[0].text);
+  }
+  });
+}
 
-document.getElementById('demo').innerHTML = url.verses[0].text;
+function start() {
+  input = document.getElementById('search').value;
+  urll = "https://api.lsm.org/recver.php?String=" + input + '&In=osis&Out=json';
+  console.log(urll);
+  getVerses(urll);
+}
+
+var url = "https://api.lsm.org/recver.php?String='John 1:1'&Out=json";
+
+$(document).ready(function(){
+   $('body').on('click', 'button', function(){
+     getVerses();
+   });
+});
+
+button.addEventListener('click', start());
 
 /*
-$.ajax({
-  url: url,
-  success: function(result){
-  document.getElementById("demo").innerHTML = (result.url);
-}
+$(document).ready(function(){
+   $('body').on('click', 'a', function(){
+     chrome.tabs.create({url: $(this).attr('href')});
+     return false;
+   });
 });
 */

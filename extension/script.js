c976eabe3a08358b51950691139e60d6b5aafdf8
MasterOfTheTiger <ted.programmer@gmail.com>
2018-02-24 16:39:02 -0800

    Add dropdown CSS and fix error message

--- extension/script.js
+++ extension/script.js
@@ -1,4 +1,5 @@
 function getVerses(reference, version) {
+  document.getElementById('result').display = 'none';
   url = 'http://api.biblia.com/v1/bible/content/' + version + '.txt?passage=' + reference + '&callback=myCallbackFunction&key=b4e7aedb44e6c74f4327182f0a244da1';
   fetch(url)
   .then(response => response.text())
@@ -11,6 +12,7 @@ function getVerses(reference, version) {
       document.getElementById('error').style.display = 'block';
       document.getElementById('error').innerHTML = 'It looks like there is a problem with your query.';
     }
+    document.getElementById('result').display = 'block';
     return result;
   });
 }
@@ -21,10 +23,12 @@ if (!navigator.onLine) {
   document.getElementById('error').style.display = 'none';
 }
 function start() {
-  var version = document.getElementById('translation').value;
-  input = document.getElementById('search').value;
-  urll = "http://api.biblia.com/v1/bible/content/KJV.txt?passage=" + input +  "&callback=myCallbackFunction&key=b4e7aedb44e6c74f4327182f0a244da1";
-  getVerses(input, version);
+input = document.getElementById('search').value;
+  if (input != '') {
+    var version = document.getElementById('translation').value;
+    urll = "http://api.biblia.com/v1/bible/content/KJV.txt?passage=" + input +  "&callback=myCallbackFunction&key=b4e7aedb44e6c74f4327182f0a244da1";
+    getVerses(input, version);
+  }  
 }
 var references =
       ["1 Thessalonians 5.16",
